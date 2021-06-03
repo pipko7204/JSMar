5a7f0a74a8e17a7426cace4e31a8dc32fe3f4011
Pavel Mamatov <pmamatov@icloud.com>
2021-06-03 16:33:37 +0300

    hw-2/1

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-alert('sdgn')
+
+const player1 = {
+    name: 'Scorpion',
+    hp: 100,
+    img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
+    weapon: ['121', '122'],
+    attack: function () {
+        console.log( this.name + 'Fight...' );
+    }
+}
+
+const player2 = {
+    name: 'Kitana',
+    hp: 100,
+    img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
+    weapon: ['121', '122'],
+    attack: function () {
+        console.log( this.name + 'Fight...' );
+    }
+}
+
+function createPlayer(player, name, hp) {
+    const $player = document.createElement("div");
+    $player.classList.add(`player`);
+
+    const $progressBar = document.createElement('div').classList.add('progressBar');
+    const $character = document.createElement('div').classList.add('character');
+
+    console.log('###: C', C);
+    
+    $player.appendChild($progressBar);
+    $player.appendChild($character);
+
+
+}
+
+createPlayer('player1', 'SCORPION', 50);
+createPlayer('player2', 'SUB-ZERO', 80);
